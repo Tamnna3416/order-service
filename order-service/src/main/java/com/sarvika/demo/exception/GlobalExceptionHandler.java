c1bbd4da439fc27690944fb9aa240cb969b17b9c
Tamanna Sharma <tamanna.sharma01@infosys.com>
2025-04-10 16:19:08 +0530

    Adding changes

---
+++ order-service/src/main/java/com/sarvika/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.sarvika.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    public ResponseEntity<String> handleOrderNotFound(OrderNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
---
+++ order-service/src/main/java/com/sarvika/demo/model/Order.java
@@ -0,0 +1,26 @@
+package com.sarvika.demo.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId; 
+    private LocalDateTime orderDate;
+    private String status;
+    private Double totalAmount;
+
+}
